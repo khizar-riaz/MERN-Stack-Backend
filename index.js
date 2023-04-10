ca10f5de89d5216794f5f2560dcf21b01d217c74
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-10 15:38:03 +0200

    check working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,resp)=>{
-    resp.send("App is working.....")
+    resp.send("App is working justto check the working.....")
 });
 
 app.listen(5000)
