d0f50a45cefdb04ea79c010f8b7f33db9aca1ebc
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-11 16:25:08 +0200

    Node js connect with MongoDB 1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
+const mongoose = require('mongoose');
 const app = express();
+const connectDB = async ()=>{
+    mongoose.connect('mongodb://localhost:27017/e-comm');   //our database connnection
+    const ProductSchema = new mongoose.Schema({});    //schema to perfome CURD //model check schema in database
+    const product = mongoose.model('product', ProductSchema);  //schema to perfome CURD //model check schema in database
+    const data = await product.find();
+    console.warn(data);
 
-app.get("/",(req,resp)=>{
-    resp.send("App is working justto check the working.....")
-});
+}
+connectDB();
+
 
 app.listen(5000)
