0217be34c9a2327eedc28d75658ce3fd7a67ac3d
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-12 23:45:30 +0200

    Signup API

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
+require("./db/config");
+const User = require("./db/User");
 const app = express();
-const connectDB = async () => {
-  mongoose.connect("mongodb://127.0.0.1:27017/e-comm"); //our database connnection instead of localhost type 127.0.0.1 as ipv4
 
-  const ProductSchema = new mongoose.Schema({}); //schema to perfome CURD //model check schema in database
-  const product = mongoose.model("product", ProductSchema); //schema to perfome CURD //model check schema in database
-  const data = await product.find();
-  console.warn(data);
-};
-connectDB();
-
+app.post("/register", (req, resp) => {
+  resp.send("api in progress....");
+});
 app.listen(5000);
-
-
