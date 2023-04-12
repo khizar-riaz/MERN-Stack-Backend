3567ef713b7bcd7a9d9cca18def8b79d06006ba9
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-12 23:28:10 +0200

    Node js connect with MongoDB updated

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
-const connectDB = async ()=>{
-    mongoose.connect('mongodb://localhost:27017/e-comm');   //our database connnection
-    const ProductSchema = new mongoose.Schema({});    //schema to perfome CURD //model check schema in database
-    const product = mongoose.model('product', ProductSchema);  //schema to perfome CURD //model check schema in database
-    const data = await product.find();
-    console.warn(data);
+const connectDB = async () => {
+  mongoose.connect("mongodb://127.0.0.1:27017/e-comm"); //our database connnection instead of localhost type 127.0.0.1 as ipv4
 
-}
+  const ProductSchema = new mongoose.Schema({}); //schema to perfome CURD //model check schema in database
+  const product = mongoose.model("product", ProductSchema); //schema to perfome CURD //model check schema in database
+  const data = await product.find();
+  console.warn(data);
+};
 connectDB();
 
+app.listen(5000);
 
-app.listen(5000)
+
