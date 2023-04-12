5f91356c2fd05c2f7cf5976fa97f0b3adc485a63
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-04-13 00:08:01 +0200

    sending josn data in the http post method body

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ require("./db/config");
 const User = require("./db/User");
 const app = express();
 
+app.use(express.json()); //sending json data in body
+
+
 app.post("/register", (req, resp) => {
-  resp.send("api in progress....");
+  resp.send(req.body);   //request json data in the http post method body
 });
 app.listen(5000);
